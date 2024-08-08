0c281189bf00e9b50b036d9f494e3bf5d92747c9
Carlos Alberto Giraldo Calderón <carlgira@Carloss-MacBook-Pro.local>
2024-08-07 23:06:08 -0500

    First app version

---
+++ Assets/Config.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{   
+    public static readonly string GEMINI_API_URL = "<GEMINI_API_URL>";
+    public static readonly string GEMINI_API_KEY = "<GEMINI_API_KEY>";
+    public static readonly string ELEVENLABS_API_KEY = "<ELEVENLABS_API_KEY>";
+    public static readonly string PORCUPINE_API_KEY = "<PORCUPINE_API_KEY>";
+}
